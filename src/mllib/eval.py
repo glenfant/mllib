444a45813312c148da5f004118bbcdd1c79deca4
Gilles Lenfant <gilles.lenfant@gmail.com>
2015-08-08 15:47:54 +0200

    JSONify the vars param is required

--- src/mllib/eval.py
+++ src/mllib/eval.py
@@ -9,6 +9,8 @@ http://docs.marklogic.com/REST/POST/v1/eval
 
 from __future__ import unicode_literals, print_function, absolute_import
 
+import json
+
 from .restclient import RESTClient
 from .utils import KwargsSerializer, ResponseAdapter, dict_pop
 
@@ -26,5 +28,7 @@ class EvalService(RESTClient):
         params, ignored = tool.request_params(kwargs)
         headers = {'Accept': 'multipart/mixed', 'Content-type': 'application/x-www-form-urlencoded'}
         data = dict_pop(params, 'xquery', 'javascript', 'vars')
+        if 'vars' in data:
+            data['vars'] = json.dumps(dict(data['vars']))
         response = self.rest_post('/v1/eval', params=params, data=data, headers=headers)
         return ResponseAdapter(response)
