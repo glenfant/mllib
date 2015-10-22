96dd2a30bb2e5c6ac15ea3301ab4805b4ad9d4d6
Gilles Lenfant <gilles.lenfant@gmail.com>
2015-10-22 14:51:21 +0200

    Added some transaction mgmt features (to be continues)

---
+++ src/mllib/transactions.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+"""
+==========
+the.module
+==========
+One line description of the module.
+
+This is an optional longer description of the module, which may
+span several lines.
+
+Or even paragraphs.
+"""
+
